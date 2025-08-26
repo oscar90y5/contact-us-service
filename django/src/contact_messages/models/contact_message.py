a4dbe2ce144731f90919cf4fec610648e0be6763
Óscar Fernández Angulo <oscarfernandezangulo@gmail.com>
2025-08-26 17:50:03 +0200

    Escribimos a un bot de telegram cuando alguien contacta.

--- django/src/contact_messages/models/contact_message.py
+++ django/src/contact_messages/models/contact_message.py
@@ -1,4 +1,6 @@
+import os
 from django.db import models
+import requests
 
 
 class ContactMessage(models.Model):
@@ -10,3 +12,21 @@ class ContactMessage(models.Model):
     message = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.notify_creation()
+
+        super().save(*args, **kwargs)
+
+    def notify_creation(self):
+        message = (f"🥳 ¡Enhorabuena, máquina!\n\n"
+                   f"Tienes un nuevo contacto en la página {self.origin}.\n\n"
+                   f"📧 Email: {self.email or 'No proporcionado'}\n"
+                   f"📞 Teléfono: {self.phone or 'No proporcionado'}\n"
+                   f"💬 Mensaje: {self.message}\n")
+
+        requests.post(f"https://api.telegram.org/bot{os.environ.get('TELEGRAM_BOT_TOKEN')}/sendMessage", data={
+            "chat_id": os.environ.get('TELEGRAM_CHAT_ID'),
+            "text": message
+        })
